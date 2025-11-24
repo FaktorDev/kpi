13d225b46adca440a657e6b5f609322560ca03d7
Alex Faktor <118138704+AlexFaktor@users.noreply.github.com>
2025-11-24 17:39:47 +0200

    check

--- lab_4_hw/lab_4_hw.cpp
+++ lab_4_hw/lab_4_hw.cpp
@@ -7,35 +7,35 @@ int main() {
     srand(time(0));
 
     int n, m;
-    cout << "Введіть n (розмір A): ";
+    cout << "Enter where we find (size A): ";
     cin >> n;
-    cout << "Введіть m (розмір B, m < n): ";
+    cout << "Enter subsequence m (size B, m < n): ";
     cin >> m;
 
-    // Виділення памʼяті під масиви
+    // Allocating memory for arrays
     int* A = new int[n];
     int* B = new int[m];
 
-    // Генерація A
+    // A
     for (int i = 0; i < n; i++)
         A[i] = rand() % 10;
 
-    // Генерація B
+    // B
     for (int i = 0; i < m; i++)
         B[i] = rand() % 10;
 
-    // Вивід
-    cout << "A: ";
+    // Conclusion
+    cout << "A:\n";
     for (int i = 0; i < n; i++)
         cout << A[i] << " ";
     cout << endl;
 
-    cout << "B: ";
+    cout << "B:\n";
     for (int i = 0; i < m; i++)
         cout << B[i] << " ";
     cout << endl;
 
-    // Пошук входження B у A
+    // Finding the occurrence of B in A
     int start = -1;
 
     for (int i = 0; i <= n - m; i++) {
@@ -55,28 +55,27 @@ int main() {
     }
 
     if (start == -1) {
-        cout << "Підпослідовність B не знайдена в A" << endl;
+        cout << "Subsequence B not found in A" << endl;
 
         delete[] A;
         delete[] B;
         return 0;
     }
 
-    // Видалення підпослідовності (ручний зсув елементів)
+    // Delete a subsequence(manual element shifting)
     for (int i = start; i < n - m; i++) {
         A[i] = A[i + m];
     }
 
-    // новий розмір масиву після видалення
+    // new array size after deletion
     int newSize = n - m;
 
-    cout << "A після видалення підпослідовності B: ";
+    cout << "A after removing subsequence B: \n";
     for (int i = 0; i < newSize; i++)
         cout << A[i] << " ";
     cout << endl;
 
-    // Звільнення памʼяті
-    // Звільнення памʼяті
+    // Freeing memory
     delete[] A;
     delete[] B;
 
